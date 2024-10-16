8a16796d3eeb4fd4ae11998104af33fc3c06a024
Alexander Åberg <alexander_aberg1985@msn.com>
2024-10-16 17:50:11 +0200

    Created a base class with the items list

---
+++ TjuvOchPolis/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TjuvOchPolis
+{
+    public class Item
+    {
+        public string Wallet { get; set; }
+        public string Keys { get; set; }
+        public string Mobile { get; set; }
+        public string Watch { get; set; }
+
+        public Item(string wallet, string keys, string mobile, string watch)
+        {
+            Wallet = wallet;
+            Keys = keys;
+            Mobile = mobile;
+            Watch = watch;
+        }
+    }
+}
